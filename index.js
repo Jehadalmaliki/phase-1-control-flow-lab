6a987390e35ccde08b6001471faa81ad03fb9788
Jehad Almaliki <yourgitemail@example.com>
2022-12-29 22:44:42 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+
+  function scuberGreetingForFeet(feet) {
+    if (feet <= 400) {
+      return 'This one is on me!';
+    } else if (feet > 400 && feet <= 2000) {
+      return 'That will be twenty bucks.';
+    } else if (feet > 2000 && feet <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+    } else {
+      return 'No can do.';
+    }
+  }
+  
+
+
+function ternaryCheckCity(city ){
+  if(city==='NYC' ){
+    return  "Ok, sounds good.";
+  }
+
+   else{   
+    return "No go.";
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
